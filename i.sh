fe84192d5596cbcad57cbb67ab2f4eb1e03ef8ce
extensionsapp <extensionsapp@gmail.com>
2018-08-08 01:58:32 +0300

    New Theme Daenerys

--- i.sh
+++ i.sh
@@ -123,11 +123,11 @@ read_theme() {
             AGAIN=no
             THEME='default'
         else
-            if [ "${THEME}" = "default" ] || [ "${THEME}" = "hodor" ] || [ "${THEME}" = "sansa" ] || [ "${THEME}" = "robb" ] || [ "${THEME}" = "ramsay" ] || [ "${THEME}" = "tyrion" ] || [ "${THEME}" = "cersei" ] || [ "${THEME}" = "joffrey" ] || [ "${THEME}" = "drogo" ] || [ "${THEME}" = "bran" ] || [ "${THEME}" = "arya" ] || [ "${THEME}" = "mormont" ] || [ "${THEME}" = "tarly" ]
+            if [ "${THEME}" = "default" ] || [ "${THEME}" = "hodor" ] || [ "${THEME}" = "sansa" ] || [ "${THEME}" = "robb" ] || [ "${THEME}" = "ramsay" ] || [ "${THEME}" = "tyrion" ] || [ "${THEME}" = "cersei" ] || [ "${THEME}" = "joffrey" ] || [ "${THEME}" = "drogo" ] || [ "${THEME}" = "bran" ] || [ "${THEME}" = "arya" ] || [ "${THEME}" = "mormont" ] || [ "${THEME}" = "tarly" ] || [ "${THEME}" = "daenerys" ]
             then
                 AGAIN=no
             else
-                printf "${R}WARNING:${NC} Нет такой темы. На данный момент существуют темы: hodor, sansa, robb, ramsay, tyrion, cersei, joffrey, drogo, bran, arya, mormont и tarly. \n"
+                printf "${R}WARNING:${NC} Нет такой темы. На данный момент существуют темы: hodor, sansa, robb, ramsay, tyrion, cersei, joffrey, drogo, bran, arya, mormont, tarly и daenerys. \n"
             fi
         fi
     done
